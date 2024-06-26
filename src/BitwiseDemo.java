a05a052c474e0fd1edbdb050727be0d9068fe703
SMarson03 <167792197+SMarson03@users.noreply.github.com>
2024-06-26 14:37:47 -0400

    4

--- src/BitwiseDemo.java
+++ src/BitwiseDemo.java
@@ -2,11 +2,12 @@ public class BitwiseDemo {
     public static void main(String [] arg){
         int x = 58; //111010
         int y = 13; //1101
-        System.out.println("x & y:" +(x & y));
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
+        System.out.println("x & y:" +(x & y)); //return 8 = 1000
+        System.out.println("x | y:" +(x | y)); //63 = 111111
+        System.out.println("x ^ y:" +(x ^ y)); //55 = 11011
+        System.out.println("~x :" +(~x)); //-59
+        System.out.println("x << y:" +(x << y));
+        System.out.println("x >> y:" +(x >> y));
+
     }
 }
