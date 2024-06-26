2031ece6fb431955369088f7e5d8f332eb38d904
SMarson03 <167792197+SMarson03@users.noreply.github.com>
2024-06-26 12:31:33 -0400

    3

---
+++ src/BitwiseDemo.java
@@ -0,0 +1,12 @@
+public class BitwiseDemo {
+    public static void main(String [] arg){
+        int x = 58; //111010
+        int y = 13; //1101
+        System.out.println("x & y:" +(x & y));
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+    }
+}
